7353f1668c9f71fedc99816128be75327682fe4b
nipunaupeksha <nipuna.upeksha@gmail.com>
2022-12-29 19:10:33 +0530

    working on header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { InlineSVGModule } from 'ng-inline-svg-2'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { Card1Component } from './components/cards/card1/card1.component'
+import { HeaderComponent } from './components/header/header.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { SigninComponent } from './pages/signin/signin.component'
 
 @NgModule({
-  declarations: [AppComponent, SigninComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    Card1Component,
+    DashboardComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    InlineSVGModule.forRoot(),
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/cards/card1/card1.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-card1',
+  templateUrl: './card1.component.html',
+  styleUrls: ['./card1.component.scss'],
+})
+export class Card1Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
