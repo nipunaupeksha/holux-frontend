c6e44c00790d222920b4b0bfe5fe653635f9bac8
nipunaupeksha <nipuna.upeksha@gmail.com>
2023-01-22 10:31:03 +0530

    pos display updated

--- src/app/components/widgets/widget6/widget6.component.ts
+++ src/app/components/widgets/widget6/widget6.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./widget6.component.scss'],
 })
 export class Widget6Component implements OnInit {
+  items = [
+    { code: 1, name: 'Fish Roll', price: 160, stock: 10, options:'add' },
+    { code: 2, name: 'Fried Rice', price: 250, stock: 3, options:'add' },
+  ]
   constructor() {}
 
   ngOnInit(): void {}
